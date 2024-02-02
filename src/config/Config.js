eaaf7a1aa0b840b661e1a34e9da6b295b0e23ed2
nooravipp-ux <nooravipp9@gmail.com>
2024-02-02 21:22:04 +0700

    first commit

---
+++ src/config/Config.js
@@ -0,0 +1,5 @@
+export const BASE_URL = 'http://154.41.229.119/camsystem-api/api';
+export const BASE_IMG_URL = 'http://154.41.229.119/camsystem';
+
+// export const BASE_URL = 'http://192.168.1.33:8000/api';
+// export const BASE_IMG_URL = 'http://localhost:8000/pamsystem';
